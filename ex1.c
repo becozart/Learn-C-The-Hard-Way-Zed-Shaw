56ce5d75ac20d10640fd80e18607f0a4292aeb2f
becozart <becozart@gmail.com>
2022-05-06 19:40:12 -0400

    Add Poem

--- ex1.c
+++ ex1.c
@@ -7,6 +7,11 @@ int main(int argc, char *argv[])
 	
 	// this is also a comment
 	printf("You are %d miles away.\n", distance);
+	printf("You are competent.\n");
+	printf("You are getting paid to learn cool shit.\n");
+	printf("You are learning fast enough.\n");
+	printf("Don't stress, enjoy.\n");
+	printf("You are %d miles away.\n", distance);
 	
 	return 0;
 }
